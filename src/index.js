a76059e55a41f5a8e9769a4fa66a3b52d5e32b14
huyhuyhuy112233 <nguyenanhhuy2004@gmail.com>
2023-05-03 21:34:43 +0700

    bt4

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ShoesStore from "./homework-3/ShoesStore";
+import FormDemo from "./homework-4/FormDemo";
+import Form from "./reactForm/Form";
+import Lifecycle from "./reactlifecycle/Lifecycle";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <ShoesStore></ShoesStore>
+  // <Form></Form>
+  <FormDemo></FormDemo>
+  // <Lifecycle></Lifecycle>
+);
