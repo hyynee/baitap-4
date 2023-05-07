8b91c895a428e2a455aaa5f9cf801478e5c7231d
huyhuyhuy112233 <nguyenanhhuy2004@gmail.com>
2023-05-07 16:22:31 +0700

    bai_tap_redux

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import FormDemo from "./homework-4/FormDemo";
 import Form from "./reactForm/Form";
 import Lifecycle from "./reactlifecycle/Lifecycle";
 
+import { Provider } from "react-redux";
+import { store } from "./redux/configStore";
+import Homepages from "./redux/Demo/Homepages";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <ShoesStore></ShoesStore>
   // <Form></Form>
-  <FormDemo></FormDemo>
+  // <FormDemo></FormDemo>
   // <Lifecycle></Lifecycle>
+  <Provider store={store}>
+    <Homepages></Homepages>
+  </Provider>
 );
